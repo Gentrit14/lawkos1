d13b02028571d8b1075590543655ec9304625b97
gentrits1 <gentritsinani75@gmail.com>
2024-08-07 16:19:19 +0200

    GENT

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.scss';
-
 import { GoLaw } from "react-icons/go";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { LuPhoneCall } from "react-icons/lu";
@@ -9,51 +8,61 @@ import { FaBars, FaTimes } from "react-icons/fa"; // Add these icons for hamburg
 import ProductsMegamenu from './ProductsMegamenu';
 
 const Nav = () => {
-  const [isMobile, setIsMobile] = useState(false);
+    const [isMobile, setIsMobile] = useState(false);
+    const [activeLink, setActiveLink] = useState(null);
 
-  const handleClick = () => {
-    setIsMobile(!isMobile);
-  };
+    const toggleMenu = () => setIsMobile(!isMobile);
+    const toggleSubMenu = (link) => {
+        if (activeLink === link) {
+            setActiveLink(null);
+        } else {
+            setActiveLink(link);
+        }
+    };
 
-  return (
-    <div className='nav'>
-        <Link to='/' className='title'><GoLaw />LAWKOS</Link>
+    return (
+        <div className='nav'>
+            <Link to='/' className='title'><GoLaw />LAWKOS</Link>
 
-        <div className={isMobile ? 'links mobile' : 'links'}>
-           <div className='nav-link'>
-              <Link to='/' className='inner-link'>About</Link>
-           </div>
-           <div className='nav-link'>
-              <Link to='/' className='inner-link'>Services <MdKeyboardArrowDown /> </Link>  
-              <ProductsMegamenu />      
-           </div>
-           <div className='nav-link'>
-              <Link to='/' className='inner-link'>Resurces <MdKeyboardArrowDown /> </Link>  
-              <ProductsMegamenu />      
-           </div>
-           <div className='nav-link'>
-              <Link to='/' className='inner-link'>News </Link>    
-           </div>
-           <div className='nav-link'>
-              <Link to='/' className='inner-link'>Careers </Link>    
-           </div>
-           <div className='auth'>
-              <div className='number'>
-                 <LuPhoneCall />
-                 <div className='number-detail'>
-                   <h6>+383 123 456 789</h6>
-                   <p>Call our office</p>
-                 </div>
-              </div>
-              <Link to='/' className='contact'>Contact us</Link>
-           </div>
+            <div className={isMobile ? 'links mobile open' : 'links'}>
+                <div className={`nav-link ${activeLink === 'about' ? 'active' : ''}`} onClick={() => toggleSubMenu('about')}>
+                    <Link to='/' className='inner-link'>About</Link>
+                </div>
+                <div className={`nav-link ${activeLink === 'services' ? 'active' : ''}`} onClick={() => toggleSubMenu('services')}>
+                    <div className="inner-link">
+                        Services <MdKeyboardArrowDown />
+                    </div>
+                    {activeLink === 'services' && <ProductsMegamenu />}
+                </div>
+                <div className={`nav-link ${activeLink === 'resources' ? 'active' : ''}`} onClick={() => toggleSubMenu('resources')}>
+                    <div className="inner-link">
+                        Resources <MdKeyboardArrowDown />
+                    </div>
+                    {activeLink === 'resources' && <ProductsMegamenu />}
+                </div>
+                <div className={`nav-link ${activeLink === 'news' ? 'active' : ''}`} onClick={() => toggleSubMenu('news')}>
+                    <Link to='/' className='inner-link'>News</Link>
+                </div>
+                <div className={`nav-link ${activeLink === 'careers' ? 'active' : ''}`} onClick={() => toggleSubMenu('careers')}>
+                    <Link to='/' className='inner-link'>Careers</Link>
+                </div>
+                <div className='auth'>
+                    <div className='number'>
+                        <LuPhoneCall />
+                        <div className='number-detail'>
+                            <h6>+383 123 456 789</h6>
+                            <p>Call our office</p>
+                        </div>
+                    </div>
+                    <Link to='/' className='contact'>Contact us</Link>
+                </div>
+            </div>
+
+            <div className='hamburger' onClick={toggleMenu}>
+                {isMobile ? <FaTimes /> : <FaBars />}
+            </div>
         </div>
-        
-        <div className='hamburger' onClick={handleClick}>
-          {isMobile ? <FaTimes /> : <FaBars />}
-        </div>
-    </div>
-  );
+    );
 };
 
 export default Nav;
