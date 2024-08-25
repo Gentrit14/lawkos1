ec36f4a018b78552f75e2622a4d0d19a115da5c1
gentrits1 <gentritsinani75@gmail.com>
2024-08-25 21:19:34 +0200

    GENT

--- src/pages/Card/data.js
+++ src/pages/Card/data.js
@@ -5,16 +5,19 @@ import Card3 from '../../img/card3.png'
 
 export const Data1 = [
   {
+      id: 0,
       img: Card1,
       name: 'Labinot Halimi',
       position: 'Managing Partner'
   },
   {
+    id: 1,
     img: Card2,
       name: 'Arbenita Jashari',
       position: 'Attorney at law, Partner'
   },
   {
+    id: 2,
       img: Card3,
       name: 'Festina Murati',
       position: 'Attorney at law'
